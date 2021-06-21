bb92dccc372981941ce88e4c0f03a0a7ec2b7dfd
WYSIWYP <stuartbyrom@gmail.com>
2021-06-21 21:23:06 +0200

    Small changes to KS and score display
    Changes to key signature display, number of measures per row, notehead sizes, and user pref defaults

---
+++ precache-manifest.136ee53e6c2b397862e1a027a8a31fa5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c6572a82aac362616552fbfa874617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "595428cdaf0d9e133189",
+    "url": "/static/css/main.422f8397.chunk.css"
+  },
+  {
+    "revision": "f69c3a231673d7e2b3fd",
+    "url": "/static/js/2.df10192f.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/2.df10192f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "163a8fb04a233f97fb50",
+    "url": "/static/js/3.df2f224b.chunk.js"
+  },
+  {
+    "revision": "3e16f7ef0279c75e8aff7b3eae6f8dac",
+    "url": "/static/js/3.df2f224b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1969d371d74887364422",
+    "url": "/static/js/4.31c1e0e8.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.31c1e0e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf5bb7f31d577023b2d9",
+    "url": "/static/js/5.55272ebd.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.55272ebd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "595428cdaf0d9e133189",
+    "url": "/static/js/main.31b22231.chunk.js"
+  },
+  {
+    "revision": "dd720463aa8e951de1d4",
+    "url": "/static/js/runtime~main.7d2262f4.js"
+  },
+  {
+    "revision": "2ec809837ce813c94da7b93fbaa4dbc3",
+    "url": "/static/media/dropDown.2ec80983.svg"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.736371ac1158803b5a9494f5b4038bd2.js"
+  "/precache-manifest.136ee53e6c2b397862e1a027a8a31fa5.js"
 );
 
 self.addEventListener('message', (event) => {
